00485c0e7e2998ecb7366dfb95b718cbb8d6aa62
Ignacio Nazzo <nachonazzo@gmail.com>
2019-08-15 23:47:23 -0300

    primer test

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-/*         'mysql' => [
+        'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -61,9 +61,9 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
-        ], */
+        ],
 
-        'mysql' => [
+/*         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
@@ -77,7 +77,7 @@ return [
             'strict' => true,
             'engine' => null,
         ],
-
+ */
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
