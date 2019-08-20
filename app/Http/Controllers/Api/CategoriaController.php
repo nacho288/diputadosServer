20e9a2e31da15b773b671fef483aa0bc6a46cafe
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2019-08-19 21:14:15 -0300

    API - CategoriaController

--- app/Http/Controllers/Api/CategoriaController.php
+++ app/Http/Controllers/Api/CategoriaController.php
@@ -3,90 +3,28 @@
 namespace App\Http\Controllers\Api;
 
 use App\Categoria;
-use App\Http\Requests\CategoriaRequest;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CategoriaController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Categoria[]|\Illuminate\Database\Eloquent\Collection
      */
     public function index()
     {
-        return view('layouts.categorias.index')->with('categorias', Categoria::all());
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('layouts.categorias.create');//
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(CategoriaRequest $request)
-    {
-        $categoria = new Categoria();
-        $categoria->nombre = $request->nombre;
-        $categoria->save();
-
-        return  view('layouts.categorias.result');
+        return Categoria::all();
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Categoria  $categoria
-     * @return \Illuminate\Http\Response
+     * @param  Categoria $categoria
+     * @return Categoria
      */
     public function show(Categoria $categoria)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Categoria  $categoria
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Categoria $categoria)
-    {
-        return view('layouts.categorias.edit')->with('categoria', $categoria);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Categoria  $categoria
-     * @return \Illuminate\Http\Response
-     */
-    public function update(CategoriaRequest $request, Categoria $categoria)
-    {
-        $categoria->update($request->all());
-
-        return redirect('/categorias');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Categoria  $categoria
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Categoria $categoria)
-    {
-        //
+        return $categoria;
     }
 }
