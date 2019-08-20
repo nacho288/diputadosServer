e333f7af9235684c1678bbf0a0b12d39acbe6446
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2019-08-19 22:39:52 -0300

    API - DocumentoController

---
+++ app/Http/Controllers/Api/DocumentoController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Documento;
+use App\Http\Controllers\Controller;
+
+class DocumentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Documento[]|\Illuminate\Database\Eloquent\Collection
+     */
+    public function index()
+    {
+        return Documento::all();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Documento $documento
+     * @return Documento
+     */
+    public function show(Documento $documento)
+    {
+        return $documento;
+    }
+}
